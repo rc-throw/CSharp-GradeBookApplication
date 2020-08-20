8573d0056fd45bf035f883f8be8fd165b6df85c2
Ryan Collamore <rcollamore@paylocity.com>
2020-08-20 18:20:48 -0400

    added calcuation for ranked grades to GetLetterGrade override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,8 @@
 ﻿using GradeBook.Enums;
 using System;
+using System.Linq;
 
- namespace GradeBook.GradeBooks
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
@@ -10,11 +11,22 @@ using System;
             Type = Enums.GradeBookType.Ranked;
         }
 
-        public override char GetLetterGrade(double averageGrade) 
+        public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work");
-            return base.GetLetterGrade(averageGrade);
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else return 'F';
         }
     }
 }
